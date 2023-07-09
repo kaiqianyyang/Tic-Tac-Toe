3f2fb7e2a92f80b8d46e08a478e1f109d1bf9f41
Kaiqian Yang <kaiqianyang@Kaiqians-MacBook-Pro-2.local>
2023-07-08 17:07:24 -0700

    add all basic operations

--- SFML_Tutorial/GameState.cpp
+++ SFML_Tutorial/GameState.cpp
@@ -23,6 +23,8 @@ namespace Sonar
         this->_data->assets.LoadTexture( "Grid Sprite", GRID_SPRITE_FILEPATH );
         this->_data->assets.LoadTexture( "X Piece", X_PIECE_FILEPATH );
         this->_data->assets.LoadTexture( "O Piece", O_PIECE_FILEPATH );
+        this->_data->assets.LoadTexture( "X Winning Piece", X_WINNING_PIECE_FILEPATH );
+        this->_data->assets.LoadTexture( "O Winning Piece", O_WINNING_PIECE_FILEPATH );
         
         _background.setTexture( this->_data->assets.GetTexture( "Background" ) );
         _pauseButton.setTexture( this->_data->assets.GetTexture( "Pause Button" ) );
@@ -32,8 +34,12 @@ namespace Sonar
         _gridSprite.setPosition( ( SCREEN_WIDTH / 2 ) - ( _gridSprite.getGlobalBounds().width / 2 ), ( SCREEN_HEIGHT / 2 ) - ( _gridSprite.getGlobalBounds().height / 2 ) );
         
         InitGridPieces();
-//        InitGridPieces();
         
+        for ( int x = 0; x < 3; x++ ) {
+            for ( int y = 0; y < 3; y++ ) {
+                gridArray[x][y] = EMPTY_PIECE;
+            }
+        }
     }
 
     void GameState::HandleInput( )
@@ -49,10 +55,14 @@ namespace Sonar
             
             if ( this->_data->input.IsSpriteClicked( this->_pauseButton, sf::Mouse::Left, this->_data->window ) )
             {
-//                std::cout << "Pause The Game" << std::endl;
-//                this->_data->machine.AddState( StateRef(new PauseState( _data )), false );
-                this->_data->machine.AddState( StateRef(new GameOverState( _data )), true );
+                this->_data->machine.AddState( StateRef(new PauseState( _data )), false );
             }
+            else if ( this->_data->input.IsSpriteClicked( this->_gridSprite, sf::Mouse::Left, this->_data->window ) )
+             {
+                 if (STATE_PLAYING == gameState) {
+                     this->CheckAndPlacePiece( );
+                 }
+             }
         }
     }
 
@@ -67,12 +77,166 @@ namespace Sonar
         
         this->_data->window.draw( this->_background );
         this->_data->window.draw( this->_pauseButton );
+        this->_data->window.draw( this->_gridSprite );
+
+        for ( int x = 0; x < 3; x++ )
+        {
+            for ( int y = 0; y < 3; y++ )
+            {
+                this->_data->window.draw( this->_gridPieces[x][y] );
+            }
+        }
         
         this->_data->window.display( );
     }
 
+    void GameState::InitGridPieces( )
+    {
+        sf::Vector2u tempSpriteSize = this->_data->assets.GetTexture( "X Piece" ).getSize( );
+        for ( int x = 0; x < 3; x++ )
+        {
+            for ( int y = 0; y < 3; y++ )
+            {
+                _gridPieces[x][y].setTexture( this->_data->assets.GetTexture( "X Piece" ) );
+                _gridPieces[x][y].setPosition( _gridSprite.getPosition( ).x + (tempSpriteSize.x * x) - 7, _gridSprite.getPosition( ).y + (tempSpriteSize.y * y) - 7 );
+                _gridPieces[x][y].setColor( sf::Color( 255, 255, 255, 0 ) );
+            }
+        }
+    }
 
+    void GameState::CheckAndPlacePiece( )
+    {
+        sf::Vector2i touchPoint = this->_data->input.GetMousePosition( this->_data->window );
+        sf::FloatRect gridSize = _gridSprite.getGlobalBounds( );
+        sf::Vector2f gapOutsideOfGrid = sf::Vector2f( ( SCREEN_WIDTH - gridSize.width ) / 2, ( SCREEN_HEIGHT - gridSize.height ) / 2 );
+        sf::Vector2f gridLocalTouchPos = sf::Vector2f( touchPoint.x - gapOutsideOfGrid.x, touchPoint.y - gapOutsideOfGrid.y );
+        sf::Vector2f gridSectionSize = sf::Vector2f( gridSize.width / 3, gridSize.height / 3 );
+        
+        int column, row;
+        
+        if ( gridLocalTouchPos.x < gridSectionSize.x )
+        {
+            column = 1;
+        }
+        else if ( gridLocalTouchPos.x < gridSectionSize.x * 2 )
+        {
+            column = 2;
+        }
+        else if ( gridLocalTouchPos.x < gridSize.width )
+        {
+            column = 3;
+        }
+        
+        if ( gridLocalTouchPos.y < gridSectionSize.y )
+        {
+            row = 1;
+        }
+        else if ( gridLocalTouchPos.y < gridSectionSize.y * 2 )
+        {
+            row = 2;
+        }
+        else if ( gridLocalTouchPos.y < gridSize.height )
+        {
+            row = 3;
+        }
+
+        if ( gridArray[column - 1][row - 1] == EMPTY_PIECE )
+        {
+            gridArray[column - 1][row - 1] = turn;
+            
+            if ( PLAYER_PIECE == turn )
+            {
+                _gridPieces[column - 1][row - 1].setTexture( this->_data->assets.GetTexture( "X Piece" ) );
+                
+                this->CheckPlayerHasWon( turn );
+                
+                turn = AI_PIECE;
+            }
+            else if ( AI_PIECE == turn )
+            {
+                _gridPieces[column - 1][row - 1].setTexture( this->_data->assets.GetTexture( "O Piece" ) );
+                
+                this->CheckPlayerHasWon( turn );
+                
+                turn = PLAYER_PIECE;
+            }
+            
+            _gridPieces[column - 1][row - 1].setColor( sf::Color( 255, 255, 255, 255 ) );
+
+        }
+    }
+
+    void GameState::CheckPlayerHasWon( int player )
+    {
+        Check3PiecesForMatch(0, 0, 1, 0, 2, 0, player);
+        Check3PiecesForMatch(0, 1, 1, 1, 2, 1, player);
+        Check3PiecesForMatch(0, 2, 1, 2, 2, 2, player);
+        Check3PiecesForMatch(0, 0, 0, 1, 0, 2, player);
+        Check3PiecesForMatch(1, 0, 1, 1, 1, 2, player);
+        Check3PiecesForMatch(2, 0, 2, 1, 2, 2, player);
+        Check3PiecesForMatch(0, 0, 1, 1, 2, 2, player);
+        Check3PiecesForMatch(0, 2, 1, 1, 2, 0, player);
+        
+        int emptyNum = 9;
+        for ( int x = 0; x < 3; x++ )
+        {
+            for ( int y = 0; y < 3; y++ )
+            {
+                if ( EMPTY_PIECE != gridArray[x][y] )
+                {
+                    emptyNum--;
+                }
+            }
+        }
+        
+        if ( 0 == emptyNum && ( STATE_WON != gameState ) && ( STATE_LOSE != gameState ) )
+        {
+            gameState = STATE_DRAW;
+        }
+        
+        if ( STATE_DRAW == gameState || STATE_LOSE == gameState || STATE_WON == gameState )
+        {
+            // show game over
+        }
+        
+        std::cout << gameState << std::endl;
+    }
+
+    void GameState::Check3PiecesForMatch( int x1, int y1, int x2, int y2, int x3, int y3, int pieceToCheck )
+    {
+        if ( pieceToCheck == gridArray[x1][y1] && pieceToCheck == gridArray[x2][y2] && pieceToCheck == gridArray[x3][y3] )
+        {
+            std::string winningPiecesStr;
+            
+            if ( O_PIECE == pieceToCheck )
+            {
+                winningPiecesStr = "O Winning Piece";
+            }
+            else
+            {
+                winningPiecesStr = "X Winning Piece";
+            }
+            
+            _gridPieces[x1][y1].setTexture( this->_data->assets.GetTexture( winningPiecesStr ) );
+            _gridPieces[x2][y2].setTexture( this->_data->assets.GetTexture( winningPiecesStr ) );
+            _gridPieces[x3][y3].setTexture( this->_data->assets.GetTexture( winningPiecesStr ) );
+            
+            if ( PLAYER_PIECE == pieceToCheck )
+            {
+                gameState = STATE_WON;
+            }
+            else
+            {
+                gameState = STATE_LOSE;
+            }
+        }
+    }
+        
 }
+
+
+
+
 
 
 
--- SFML_Tutorial/GameState.hpp
+++ SFML_Tutorial/GameState.hpp
@@ -16,8 +16,14 @@ namespace Sonar
         void HandleInput( );
         void Update( float dt );
         void Draw( float dt );
+        
     private:
         void InitGridPieces( );
+        
+        void CheckAndPlacePiece( );
+        
+        void CheckPlayerHasWon( int turn );
+        void Check3PiecesForMatch( int x1, int y1, int x2, int y2, int x3, int y3, int pieceToCheck );
         
         GameDataRef _data;
         
